9d5bc9ba11b7593ca154b0039c574d7042bc15c2
jivanrod <jivanrod@gmail.com>
2024-10-13 22:52:43 +0200

    uodare

--- index.php
+++ index.php
@@ -1,3 +1,93 @@
+<?php
+if (filter_has_var(INPUT_POST, "enviar")) {
+    $datos = [];
+// Lectura, saneamiento y validación del dato de nombre
+// 3 a 25 caracteres en mayúsculas y minúsculas y espacio en blanco
+    $nombre = [];
+    $nombre['form'] = filter_input(INPUT_POST, 'nombre', FILTER_UNSAFE_RAW);
+    $nombre['san'] = filter_var($nombre['form'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $nombre['err'] = filter_var($nombre['san'], FILTER_VALIDATE_REGEXP,
+                    ['options' => ['regexp' => "/^[a-z A-Záéíóúñ]{3,25}$/"]]) === false;
+
+    $datos['nombre'] = $nombre;
+// Lectura, saneamiento y validación del dato de contraseña
+// 6 a 8 caracteres con mayúsculas, minúsculas, digitos y los símbolos !@#$%^&*()+
+
+    $clave = [];
+    $clave['form'] = filter_input(INPUT_POST, 'clave', FILTER_UNSAFE_RAW);
+    $clave['san'] = filter_var($clave['form'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $clave['err'] = filter_var($clave['san'], FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => "/^[\w!@#\$%\^&\*\(\)\+]{6,8}$/"]]) === false;
+
+    $datos['clave'] = $clave;
+// Lectura, saneamiento y validación del dato de correo
+// Formato correcto de correo
+
+    $correo = [];
+    $correo['form'] = filter_input(INPUT_POST, 'correo', FILTER_UNSAFE_RAW);
+    $correo['san'] = filter_var($correo['form'], FILTER_SANITIZE_EMAIL);
+    $correo['err'] = filter_var($correo['san'], FILTER_VALIDATE_EMAIL) === false;
+
+    $datos['correo'] = $correo;
+// Lectura del dato de fecha. La fecha ya viene validada del formulario
+
+    $fechaNac = [];
+    $fechaNac['form'] = filter_input(INPUT_POST, 'fechanac', FILTER_UNSAFE_RAW);
+    /*  $fechaNacError = filter_input(INPUT_POST, 'campo', FILTER_VALIDATE_REGEXP, [
+      "options" => ["regexp" => "/^.+$/"]]) === false; */
+    $fechaNac['err'] = empty($fechaNac['form']);
+
+// Lectura, saneamiento y validación del dato de telefono
+// Números sin blancos    
+
+    $tel = [];
+    $tel['form'] = filter_input(INPUT_POST, 'tel', FILTER_UNSAFE_RAW);
+    $tel['san'] = filter_var($tel['form'], FILTER_SANITIZE_NUMBER_INT);
+    $tel['err'] = (strlen($tel['san']) < 8 || strlen($tel['san']) > 11);
+
+    $datos['tel'] = $tel;
+// Lectura del dato de tienda
+    $tienda = [];
+    $tienda['form'] = filter_input(INPUT_POST, 'tienda');
+
+    $datos['tienda'] = $tienda;
+// Lectura, saneamiento y validación del dato de edad. Solo pueden acceder mayores de edad
+
+    $edad = [];
+
+    $edad['form'] = filter_input(INPUT_POST, 'edad', FILTER_UNSAFE_RAW);
+    $edad['san'] = filter_var($edad['form'], FILTER_SANITIZE_NUMBER_INT);
+    $edad['err'] = filter_var($edad['san'], FILTER_VALIDATE_INT, [
+                "options" => [
+                    "min_range" => 18,
+                    "max_range" => 120,
+                ]
+            ]) === false;
+
+    $datos['edad'] = $edad;
+
+// Lectura del dato de idioma.
+    $idioma = [];
+
+    $idioma['form'] = filter_input(INPUT_POST, 'idioma', FILTER_UNSAFE_RAW);
+    $idioma['err'] = empty($idioma['form']);
+
+    $datos['idioma'] = $idioma;
+
+    // Lectura del dato de intereses
+    $intereses = [];
+    $intereses['form'] = implode(', ', filter_input(INPUT_POST, 'intereses', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY) ?? []);
+    $datos['intereses'] = $intereses;
+
+// Validación del dato de suscripcion. Se convierte la respuesta a un valor booleano
+    $suscripcion = [];
+
+    $suscripcion['form'] = filter_input(INPUT_POST, 'suscripcion', FILTER_VALIDATE_BOOLEAN) ?? false;
+    $suscripcion['san'] = $suscripcion['form'] ? 'si' : 'no';
+
+    $datos['suscripcion'] = $suscripcion;
+}
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -26,12 +116,16 @@
                             <input id="correo" type="text"  name="correo" placeholder="Introduce el correo"  />
                         </div>
                         <div class="form-section">
-                            <label for="fechanac">Fecha de nacimiento:</Label>
-                            <input id="fechanac" type="date" name="fechanac" placeholder="Introduce la fecha de nacimiento" />
-                        </div>
-                        <div class="form-section">
                             <label for="telefono">Teléfono:</Label> 
                             <input id="telefono" type="tel" name="tel" placeholder="Introduce el teléfono" />
+                        </div>
+                        <div class="form-section">
+                            <label for="edad">Edad:</label> 
+                            <input id="edad" type="number" name="edad" placeholder="Introduce tu edad" />
+                        </div>
+                        <div class="form-section">
+                            <label for="fechanac">Fecha de nacimiento:</Label>
+                            <input id="fechanac" type="date" name="fechanac" placeholder="Introduce la fecha de nacimiento" />
                         </div>
                         <div class="form-section">
                             <label for="tienda">Tienda Preferida:</Label> 
@@ -40,10 +134,6 @@
                                 <option value="Barcelona">Barcelona</option>
                                 <option value="Valencia">Valencia</option>
                             </select> 
-                        </div>
-                        <div class="form-section">
-                            <label for="edad">Edad:</label> 
-                            <input id="edad" type="number" name="edad" placeholder="Introduce tu edad" />
                         </div>
                         <div class="form-section">
                             <label>Idioma preferido:</label>
@@ -55,6 +145,23 @@
                                 <div>
                                     <input id="inglés" type="radio" name="idioma" value="inglés" /> 
                                     <label for="inglés">Inglés</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-section">
+                            <label>Intereses:</label>
+                            <div class="select-section">
+                                <div>
+                                    <input id="deportes" type="checkbox" name="intereses[]" value="deportes" />
+                                    <label for="deportes">Deportes</label>
+                                </div>
+                                <div>
+                                    <input id="musica" type="checkbox" name="intereses[]" value="musica" />
+                                    <label for="musica">Música</label>
+                                </div>
+                                <div>
+                                    <input id="lectura" type="checkbox" name="intereses[]" value="lectura" />
+                                    <label for="lectura">Lectura</label>
                                 </div>
                             </div>
                         </div>
@@ -82,6 +189,15 @@
                         <th>Valor válido/ No válido</th>
                         <th>Valor htmlspecialchars</th>
                     </tr>
+                    <?php foreach ($datos as $dato => $valores): ?>
+                        <tr>
+                            <td><?= $dato ?></td>
+                            <td><?= $valores['form'] ?? '' ?></td>
+                            <td><?= $valores['san'] ?? '' ?></td>
+                            <td><?= ($valores['err'] ?? false) ? "$dato no es válido" : "$dato es válido" ?></td>
+                            <td><?= htmlspecialchars($valores['form'] ?? '') ?></td>
+                        </tr>
+                    <?php endforeach ?>
                 </table>
                 <a href="<?= $_SERVER['PHP_SELF'] ?>" class="submit">Volver al formulario</a>
             </div>
